34919987e55fbbbfce36047484cc4e8069f22445
Yair Zvuloon <yair.zvuloon@cymotive.com>
2020-09-27 03:21:52 +0300

    Updates

---
+++ precache-manifest.a5d767a21250ac91567953c08238cfe4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073695a6fbc925e258de9068f71ff972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "464160b613e51419da84",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ad911321d7e7115cfed8",
+    "url": "/static/css/main.5a136b3f.chunk.css"
+  },
+  {
+    "revision": "464160b613e51419da84",
+    "url": "/static/js/2.00626884.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00626884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad911321d7e7115cfed8",
+    "url": "/static/js/main.ff3f0dc5.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
