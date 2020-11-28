c4baa6c3443ff504c615f8c6dc254d5ae83e5df9
Yair Zvuloon <yair.zvuloon@cymotive.com>
2020-11-28 22:41:19 +0200

    refactor: change old promise syntax to async await

--- src/actions.js
+++ src/actions.js
@@ -7,10 +7,13 @@ export const REQUEST_ROBOTS_FAILED = 'REQUEST_ROBOTS_FAILED';
 
 export const setSearchField = text => ({ type: CHANGE_SEARCH_FIELD, payload: text })
 
-export const requestRobots = () => dispatch => {
-  dispatch({ type: REQUEST_ROBOTS_PENDING })
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
-    .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }))
+export const requestRobots = () => async dispatch => {
+  try {
+    dispatch({ type: REQUEST_ROBOTS_PENDING });
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const payload = await res.json();
+    dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload })
+  } catch (error) {
+    dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error });
+  }
 }
